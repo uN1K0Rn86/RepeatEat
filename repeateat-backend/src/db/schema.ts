29bcd84de9485c06dc45ff9f5c26f1697111258a
Sami Heinimo <sami.heinimo@helsinki.fi>
2026-01-14 13:34:14 +0200

    Work on db schema

--- repeateat-backend/src/db/schema.ts
+++ repeateat-backend/src/db/schema.ts
@@ -1,6 +1,22 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core'
+import { integer, real, pgTable, varchar } from 'drizzle-orm/pg-core'
 
-export const recipesTable = pgTable('recipes', {
+export const recipes = pgTable('recipes', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
+})
+
+export const ingredients = pgTable('ingredients', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+})
+
+export const recipeIngredients = pgTable('recipe_ingredients', {
+  recipeId: integer('recipe_id')
+    .notNull()
+    .references(() => recipes.id, { onDelete: 'cascade' }),
+  ingredientId: integer('ingredient_id')
+    .notNull()
+    .references(() => ingredients.id, { onDelete: 'cascade' }),
+  quantity: real().notNull(),
+  unit: varchar({ length: 255 }).notNull(),
 })
