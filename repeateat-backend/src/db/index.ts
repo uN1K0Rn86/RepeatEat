f9015edfdeb94bb01cf4cfe2eb19218d0b4d23be
Sami Heinimo <sami.heinimo@helsinki.fi>
2026-01-26 14:15:33 +0200

    Add backend routes for recipes and ingredients

--- repeateat-backend/src/db/index.ts
+++ repeateat-backend/src/db/index.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/node-postgres'
 
+import * as schema from './schema'
+
 const dbUrl =
   process.env.NODE_ENV === 'test'
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL
 
-const db = drizzle(dbUrl!)
+const db = drizzle(dbUrl!, { schema })
 
 export default db
--- repeateat-backend/src/routers/recipe.ts
+++ repeateat-backend/src/routers/recipe.ts
@@ -1,9 +1,21 @@
 import express, { Request, Response } from 'express'
 
 import db from '../db'
-import { recipe } from '../db/schema'
+import { recipe, ingredient } from '../db/schema'
 
 const recipeRouter = express.Router()
+
+recipeRouter.get('/', async (req: Request, res: Response) => {
+  const allRecipes = await db.query.recipe.findMany({
+    with: {
+      ingredients: true,
+      steps: true,
+    },
+    orderBy: (recipe, { asc }) => [asc(recipe.name)],
+  })
+
+  res.json(allRecipes)
+})
 
 recipeRouter.post('/', async (req: Request, res: Response) => {
   const name: string = req.body.name
@@ -13,6 +25,23 @@ recipeRouter.post('/', async (req: Request, res: Response) => {
     .values({ name: name, authorId: authorId })
     .returning()
   res.json(addedRecipe)
+})
+
+recipeRouter.get('/ingredient', async (_req: Request, res: Response) => {
+  const allIngredients = await db.query.ingredient.findMany({
+    orderBy: (ingredient, { asc }) => [asc(ingredient.name)],
+  })
+
+  res.json(allIngredients)
+})
+
+recipeRouter.post('/ingredient', async (req: Request, res: Response) => {
+  const name: string = req.body.name
+  const addedIngredient = await db
+    .insert(ingredient)
+    .values({ name: name })
+    .returning()
+  res.json(addedIngredient)
 })
 
 export default recipeRouter
