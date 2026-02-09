6a385948fed5e4bb4b2a9a9484dea7a7d6af8ee2
Sami Heinimo <sami.heinimo@helsinki.fi>
2026-02-09 16:21:29 +0200

    Update worklogs, start modifying ingredient form

--- repeateat-frontend/src/components/views/recipe/RecipeDetailsView/IngredientsView.tsx
+++ repeateat-frontend/src/components/views/recipe/RecipeDetailsView/IngredientsView.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Trash2 } from 'lucide-react'
 import { type UpdateRecipe } from '@repeateat/shared'
 import { useFieldArray, useFormContext } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
@@ -11,14 +13,14 @@ const IngredientsView = ({ editable }: IngredientProps) => {
   const { t } = useTranslation(['recipe', 'common'])
   const { register, control } = useFormContext<UpdateRecipe>()
 
-  const { fields } = useFieldArray({
+  const { fields, remove } = useFieldArray({
     control,
     name: 'ingredients',
   })
 
   return (
     <div className="w-full overflow-x-auto">
-      <table className="w-full border-collapse mx-auto text-xs sm:text-sm md:text-base">
+      <table className="w-full table-fixed border-collapse mx-auto text-xs sm:text-sm md:text-base">
         <thead>
           <tr className="even:bg-muted m-0 border-t p-0">
             <th className="border px-4 py-2 text-left font-bold w-1/2">
@@ -30,12 +32,15 @@ const IngredientsView = ({ editable }: IngredientProps) => {
             <th className="border px-4 py-2 text-left font-bold">
               {t('recipe:unit')}
             </th>
+            {editable && (
+              <th className="border px-4 py-2 text-left font-bold"></th>
+            )}
           </tr>
         </thead>
         <tbody>
           {fields.map((field, index) => (
             <tr key={field.id} className="even:bg-muted m-0 border-t p-0">
-              <th className="border px-4 py-2 text-left w-1/2">
+              <td className="border px-4 py-2 text-left w-1/2">
                 {editable ? (
                   <Input
                     {...register(`ingredients.${index}.ingredient.name`)}
@@ -44,8 +49,8 @@ const IngredientsView = ({ editable }: IngredientProps) => {
                 ) : (
                   field.ingredient.name
                 )}
-              </th>
-              <th className="border px-4 py-2 w-1/4">
+              </td>
+              <td className="border px-2 py-2 w-1/6">
                 {editable ? (
                   <Input
                     {...register(`ingredients.${index}.quantity`)}
@@ -54,8 +59,8 @@ const IngredientsView = ({ editable }: IngredientProps) => {
                 ) : (
                   field.quantity
                 )}
-              </th>
-              <th className="border px-2 py-2 w-1/4">
+              </td>
+              <td className="border px-2 py-2 w-1/6">
                 {editable ? (
                   <Input
                     {...register(`ingredients.${index}.unit`)}
@@ -64,7 +69,22 @@ const IngredientsView = ({ editable }: IngredientProps) => {
                 ) : (
                   field.unit
                 )}
-              </th>
+              </td>
+              {editable && (
+                <td className="border w-10 text-center">
+                  <Button
+                    className="w-6 h-6"
+                    type="button"
+                    variant="outline"
+                    onClick={() =>
+                      window.confirm(t('common:confirm_delete')) &&
+                      remove(index)
+                    }
+                  >
+                    <Trash2 className="w-4 h-4 text-muted-foreground" />
+                  </Button>
+                </td>
+              )}
             </tr>
           ))}
         </tbody>
