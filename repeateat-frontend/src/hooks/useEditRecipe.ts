ad02178fc2779082dff5cb01e5de55a3013614bb
Sami Heinimo <sami.heinimo@helsinki.fi>
2026-02-11 13:57:19 +0200

    Invalidate queries so edited data appears immediately

--- repeateat-frontend/src/hooks/useEditRecipe.ts
+++ repeateat-frontend/src/hooks/useEditRecipe.ts
@@ -1,15 +1,23 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 import recipeService from '@/services/recipes'
 import type { UpdateRecipe } from '@repeateat/shared'
 
 export const useEditRecipe = () => {
+  const queryClient = useQueryClient()
+
   return useMutation({
     mutationFn: (recipeToEdit: UpdateRecipe) => {
       return recipeService.editRecipe(recipeToEdit)
     },
-    onSuccess: () => {
+    onSuccess: async (data) => {
       console.log('Recipe edited')
+      await queryClient.invalidateQueries({
+        queryKey: ['recipe', data.id.toString()],
+      })
+      await queryClient.invalidateQueries({
+        queryKey: ['recipes'],
+      })
     },
   })
 }
