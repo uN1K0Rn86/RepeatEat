c70ff7d0b798333bdfe0808f9ea4ffcc31f326fb
Sami Heinimo <sami.heinimo@helsinki.fi>
2026-02-12 11:06:40 +0200

    Move user dropdown menu to separate component, move logout logic to separate hook

--- repeateat-frontend/src/components/navigation/TopAppBar.tsx
+++ repeateat-frontend/src/components/navigation/TopAppBar.tsx
@@ -1,40 +1,20 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-import { authClient } from '../../utils/auth-client'
 import { useBoundStore } from '../../store'
-import { notify } from '../../utils/notify'
 import { Button } from '../ui/button'
 import {
   DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Menu } from 'lucide-react'
-import RecipeDropdownMenu from '../views/recipe/RecipeDropdownMenu'
+import RecipeDropdownMenu from './RecipeDropdownMenu'
 import ThemeToggle from '../ThemeToggle'
 import { useTranslation } from 'react-i18next'
+import UserDropdownMenu from './UserDropdownMenu'
 
 const TopAppBar = () => {
   const { user, pageTitle } = useBoundStore()
-  const navigate = useNavigate()
   const { t } = useTranslation(['common', 'notify'])
-
-  const logOut = async () => {
-    await authClient.signOut({
-      fetchOptions: {
-        onSuccess: () => {
-          const { clearAuth } = useBoundStore.getState()
-          clearAuth()
-          notify.success(t('notify:logout', { username: user?.name }))
-          void navigate('/')
-        },
-      },
-    })
-  }
 
   return (
     <header className="sticky top-0 z-50 w-full border-b">
@@ -50,46 +30,7 @@ const TopAppBar = () => {
         <h1 className="font-bold">{t(`common:${pageTitle}`)}</h1>
 
         {user ? (
-          <div className="flex gap-2">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  variant="secondary"
-                  className="relative h-10 w-10 rounded-full"
-                >
-                  <Avatar className="h-10 w-10">
-                    {user.image && (
-                      <AvatarImage src={user.image} alt={user.name} />
-                    )}
-                    <AvatarFallback>{user.name?.charAt(0)}</AvatarFallback>
-                  </Avatar>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56" align="end" forceMount>
-                <DropdownMenuLabel className="font-normal">
-                  <div className="flex flex-col space-y-1">
-                    <p className="text-sm font-medium leading-none truncate">
-                      {user.name}
-                    </p>
-                    <p className="text-xs leading-none text-muted-foreground truncate">
-                      {user.email}
-                    </p>
-                  </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem onClick={() => void navigate('/profile')}>
-                  {t('common:profile')}
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  onClick={() => void logOut()}
-                  className="text-destructive focus:text-destructive"
-                >
-                  {t('common:logout')}
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-            <ThemeToggle />
-          </div>
+          <UserDropdownMenu user={user} />
         ) : (
           <div className="flex gap-2">
             <Button asChild variant="secondary" size="sm">
@@ -98,9 +39,9 @@ const TopAppBar = () => {
             <Button asChild variant="secondary" size="sm">
               <Link to="/login">{t('common:login')}</Link>
             </Button>
-            <ThemeToggle />
           </div>
         )}
+        <ThemeToggle />
       </div>
     </header>
   )
---
+++ repeateat-frontend/src/components/navigation/UserDropdownMenu.tsx
@@ -0,0 +1,62 @@
+import type { User } from 'better-auth'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '../ui/button'
+import { useNavigate } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { useLogout } from '@/hooks/useLogout'
+
+interface UserDropdownMenuProps {
+  user: User
+}
+
+const UserDropdownMenu = ({ user }: UserDropdownMenuProps) => {
+  const navigate = useNavigate()
+  const { t } = useTranslation(['common'])
+  const { logout } = useLogout()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="secondary" className="relative h-10 w-10 rounded-full">
+          <Avatar className="h-10 w-10">
+            {user.image && <AvatarImage src={user.image} alt={user.name} />}
+            <AvatarFallback>{user.name?.charAt(0)}</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none truncate">
+              {user.name}
+            </p>
+            <p className="text-xs leading-none text-muted-foreground truncate">
+              {user.email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => void navigate('/profile')}>
+          {t('common:profile')}
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => logout()}
+          className="text-destructive focus:text-destructive"
+        >
+          {t('common:logout')}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default UserDropdownMenu
