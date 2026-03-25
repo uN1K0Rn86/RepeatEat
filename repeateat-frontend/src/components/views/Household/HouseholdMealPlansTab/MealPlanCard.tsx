cefbf50ef196b0c0508b54675243826d1525ece1
Sami Heinimo <sami.heinimo@helsinki.fi>
2026-03-25 14:55:00 +0200

    Format mealplancard

--- repeateat-frontend/src/components/views/Household/HouseholdMealPlansTab/MealPlanCard.tsx
+++ repeateat-frontend/src/components/views/Household/HouseholdMealPlansTab/MealPlanCard.tsx
@@ -1,13 +1,16 @@
+import { Button } from '@/components/ui/button'
 import { useUserInfo } from '@/hooks/useUser'
 import type { MealPlan } from '@repeateat/shared'
 import { useTranslation } from 'react-i18next'
+import { Link, useNavigate } from 'react-router-dom'
 
 interface MealPlanCardProps {
   mealPlan: MealPlan
 }
 
 const MealPlanCard = ({ mealPlan }: MealPlanCardProps) => {
-  const { t } = useTranslation(['common'])
+  const navigate = useNavigate()
+  const { t } = useTranslation(['common', 'recipe'])
   const {
     data: createdByUser,
     isLoading,
@@ -18,9 +21,18 @@ const MealPlanCard = ({ mealPlan }: MealPlanCardProps) => {
   if (isError) return <div>Error...</div>
   if (!createdByUser) return <div>Cannot find user</div>
 
-  console.log(mealPlan)
+  const startDate = new Date(mealPlan.startDate)
+  const endDate = new Date(mealPlan.endDate)
+
+  const startText = Number.isNaN(startDate.getTime())
+    ? '-'
+    : startDate.toLocaleDateString()
+  const endText = Number.isNaN(endDate.getTime())
+    ? '-'
+    : endDate.toLocaleDateString()
+
   return (
-    <div className="p-3 border rounded-lg bg-muted space-y-3">
+    <div className="flex flex-col p-3 border rounded-lg bg-muted space-y-3">
       <div className="flex flex-row justify-between">
         <div className="font-bold underline underline-offset-2">
           {mealPlan.name}
@@ -29,8 +41,31 @@ const MealPlanCard = ({ mealPlan }: MealPlanCardProps) => {
           {t('common:created_by')}: {createdByUser.name}
         </div>
       </div>
-      <div>moi</div>
-      <div>m</div>
+      <div>
+        {t('common:valid')}: {startText} - {endText}
+      </div>
+      <div>
+        <div className="mb-2 text-sm font-medium">{t('common:recipes')}</div>
+
+        <div className="flex flex-wrap justify-between gap-2">
+          {mealPlan.mealPlanItems.map((item) => (
+            <Link
+              key={item.id}
+              to={item.recipeId ? `/recipe/${item.recipeId}` : '#'}
+              className="rounded-full bg-foreground border px-1 py-1.5 text-sm text-background hover:bg-accent transition-colors"
+            >
+              {item.recipe?.name ?? item.title ?? t('common:unknown')}
+            </Link>
+          ))}
+        </div>
+      </div>
+      <Button
+        variant="accent"
+        onClick={() => void navigate(`/household/meal-plans/${mealPlan.id}`)}
+        className="flex-1"
+      >
+        {t('recipe:view_details')} / {t('common:edit')}
+      </Button>
     </div>
   )
 }
